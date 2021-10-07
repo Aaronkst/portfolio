4df49301dd53c3a0abb49fc6bb57aebeeacc7ce2
Aaronkst <aaron.kst99@gmail.com>
2021-10-07 12:17:47 +0630

    Beautify
    - Added Animations
    - Optimized Elements for better design
    - Added link for email to avoid confusion

--- src/Header.js
+++ src/Header.js
@@ -45,57 +45,59 @@ function Header() {
   return (
     <div>
       <Button variant="dark" className="NavButton" size="lg" href={section} onClick={() => {declareSection(section)}}>{buttonText}</Button>
-      <Navbar fixed="top" className="TopNavBar" variant="dark" expand="lg">
-        <Container>
-          <Navbar.Brand href="#home" onClick={() => {declareSection('#contactme')}}>Aaron's Portfolio</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#aboutme" onClick={() => {declareSection('#home')}}>About Me</Nav.Link>
-              <Nav.Link href="#skillset" onClick={() => {declareSection('#aboutme')}}>My Skillset</Nav.Link>
-              <Nav.Link href="#project" onClick={() => {declareSection('#skillset')}}>Sample Projects</Nav.Link>
-              <Nav.Link href="#contactme" onClick={() => {declareSection('#project')}}>Contact Me</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
       <div className="Main" id="home">
+        <Navbar fixed="top" className="TopNavBar" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home" onClick={() => {declareSection('#contactme')}}>Aaron's Portfolio</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#aboutme" onClick={() => {declareSection('#home')}}>About Me</Nav.Link>
+                <Nav.Link href="#skillset" onClick={() => {declareSection('#aboutme')}}>My Skillset</Nav.Link>
+                <Nav.Link href="#project" onClick={() => {declareSection('#skillset')}}>Sample Projects</Nav.Link>
+                <Nav.Link href="#contactme" onClick={() => {declareSection('#project')}}>Contact Me</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
         <header className="Main-header">
           <img src={logo} className="Main-logo" alt="logo" />
-          <p>
-            <code># Eat, Sleep, Code, Repeat</code>
-          </p>
+          <code className="h3"><span># Eat,</span> <span>Sleep,</span> <span>Code,</span> <span>Repeat</span></code>
         </header>
       </div>
       <div className="Main-white Main-body" id="aboutme">
-        <p className="text-center h1 text-warning">About Me</p>
-        <div className="col-sm-4 mx-auto info-box">
-          <p className='text-center'>Hi! I'm <strong className='text-primary'>Khant Si Thu</strong> and you can call me <strong className='strong text-primary'>Aaron</strong>. I'm a Pices born on <strong className='strong text-primary'>11th March 1999</strong> and I live in <strong className='strong text-primary'>Yangon</strong>. My hobby is obviously (duh~) coding and developing from <strong className='strong text-primary'>complex webapps</strong> with huge scalability and database to <strong className='strong text-primary'>simple one page sites</strong> (Just like this very one)! You will be able to browse through my pervious works below. Hope you enjoy your stay</p>
+        <p className="text-center display-4 text-warning">About Me</p>
+        <div className="col-sm-4 mx-auto info-box shadow">
+          <p className='text-center'>Hi! I'm <strong className='text-primary'>Khant Si Thu</strong> and you can call me <strong className='strong text-primary'>Aaron</strong>. I'm a Pices born on <strong className='strong text-primary'>11th March 1999</strong> and I live in <strong className='strong text-primary'>Yangon</strong>. My hobby is obviously (duh~) coding and developing from <strong className='strong text-primary'>complex webapps</strong> with huge scalability and database to <strong className='strong text-primary'>simple one page sites</strong> (Just like this very one)! You will be able to browse through my sample works below. Hope you enjoy your stay</p>
         </div>
       </div>
       <div className="Main-body Main" id="skillset">
-        <p className='text-warning text-center h1'>My skillset</p>
+        <p className='text-warning text-center display-4'>My skillset</p>
         <div className="col-sm-4 my-2 offset-sm-4 px-4">
           <p className='text-white'>
             <span className="h6">Databases</span><br/>
             <code><b>No-SQL</b>: Firebase, Mongodb <br/><b>SQL</b>: MySQL</code>
           </p>
+          <p className='text-center'>
           <img className="skill-icon" src={mongodb} alt="mongodb" />
           <img className="skill-icon" src={mysql} alt="mysql" />
           <img className="skill-icon" src={firebase} alt="firebase" />
+          </p>
         </div>
         <div className="col-sm-4 my-2 offset-sm-4 px-4">
           <p className='text-white'>
             <span className="h6">Language and Frameworks</span><br/>
-            <code><b>Language</b>: NodeJS, PHP <br/><b>Backend</b>: ExpressJS<br/><b>Frontend</b>: ReactJS, ReactNative, Social Media Chatbots<br/><b>Frameworks</b>: SailsJS, NextJS, EJS</code>
+            <code><b>Language</b>: NodeJS, PHP <br/><b>Backend</b>: ExpressJS<br/><b>Frontend</b>: ReactJS, ReactNative, Social Media Chatbots<br/><b>Frameworks</b>: SailsJS, NextJS, EJS, Redux</code>
           </p>
+          <p className='text-center'>
           <img className="skill-icon" src={nodejs} alt="nodejs" />
           <img className="skill-icon" src={react} alt="react" />
           <img className="skill-icon" src={chatbot} alt="chatbot" />
+          </p>
         </div>
       </div>
       <div className="Main-body Main-white text-dark" id="project">
-        <p className='text-warning text-center h1'>Sample Projects</p>
+        <p className='text-warning text-center display-4'>Sample Projects</p>
         <div className="col-sm-6 offset-sm-3">
           <div className='row'>
             <Card style={{ maxWidth: '20rem' }} className="mx-auto my-2 shadow">
@@ -103,10 +105,10 @@ function Header() {
               <Card.Body>
                 <Card.Title>Viber Bot</Card.Title>
                 <Card.Text>
-                  <small>A simple chatbot with minimal features made to showcase my expertise in that area. Please give it a try.</small>
+                  <small>A simple chatbot with minimal features made to showcase my expertise in that area. Give it a try.</small>
                 </Card.Text>
                 <Button variant="warning" target="_blank" href="viber://pa?chatURI=aaroncb&context=portfolio&text=Hello">Visit Project &#8594;</Button>
-                <Button variant="secondary" className="mx-2" target="_blank" href="https://github.com/Aaronkst/viberchatbot">Github &#8594;</Button>
+                <Button variant="secondary" className="mx-2" target="_blank" rel="noreferrer" href="https://github.com/Aaronkst/viberchatbot">Github &#8594;</Button>
               </Card.Body>
             </Card>
             <Card style={{ maxWidth: '20rem' }} className="mx-auto my-2 shadow">
@@ -114,9 +116,9 @@ function Header() {
               <Card.Body>
                 <Card.Title>Portfolio</Card.Title>
                 <Card.Text>
-                  <small>Yes, this very portfolio is made with create-react-app. Check the Github repostiory for more details!</small>
+                  <small>Yes, this very portfolio is made with create-react-app. Check the Github repository for more details!</small>
                 </Card.Text>
-                <Button variant="secondary" target="_blank" href="https://github.com/Aaronkst/portfolio">Github &#8594;</Button>
+                <Button variant="secondary" target="_blank" rel="noreferrer" href="https://github.com/Aaronkst/portfolio">Github &#8594;</Button>
               </Card.Body>
             </Card>
           </div>
@@ -124,14 +126,15 @@ function Header() {
       </div>
       <div className="Main-body Main" id="contactme">
 
-        <p className='text-warning text-center h1'>Contact Me</p>
-        <p className='text-white text-center h5'>Don't be a stranger, Lets work together! Reach me out at</p>
+        <p className='text-warning text-center display-4'>Contact Me</p>
+        <p className='text-white text-center h6'>Don't be a stranger, Lets work together! Reach me out at</p>
         <br/>
         <p className='text-center'>
-          <span className='text-white h6'>Email</span><br/><a href='mailto:aaron.kst99@gmail.com' target="_blank" className='text-decoration-none'><code className="h5"><FontAwesomeIcon icon={faEnvelope} /> aaron.kst99@gmail.com</code></a>         
+          <span className='text-white'>Email </span><br/><a href='mailto:aaron.kst99@gmail.com' target="_blank" rel="noreferrer" className='text-decoration-none'><code className="h5"><FontAwesomeIcon icon={faEnvelope} className="faflip" /> aaron.kst99@gmail.com</code><br/><small className="text-info"> - Click to send a mail now! - </small></a>         
         </p>
+        <br/>
         <p className='text-center'>
-          <span className='text-white h6'>Phone</span><br/><code className="h5"><FontAwesomeIcon icon={faMobile} /> (+95) 9 976 328 625 <br/><FontAwesomeIcon icon={faMobile} /> (+95) 9 250 154 050</code>
+          <span className='text-white'>Phone</span><br/><code className="h5"><FontAwesomeIcon icon={faMobile} className="faflip" /> (+95) 9 976 328 625 <br/><FontAwesomeIcon icon={faMobile} className="faflip" /> (+95) 9 250 154 050</code>
         </p>
       </div>
     </div>
